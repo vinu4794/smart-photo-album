391bc4057c9aff76d1a9ded1773ab5021f643ce4
vinu4794 <79016213+vinu4794@users.noreply.github.com>
2022-08-17 17:01:35 -0400

    Update index-photos.py

--- Lambdas/index-photos.py
+++ Lambdas/index-photos.py
@@ -5,23 +5,9 @@ import requests
 import base64
 from requests_aws4auth import AWS4Auth
 
-#hey testing new changes
-# credentials = boto3.Session().get_credentials()
-# awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
-
 import botocore.response as br
 
 def rekognition_function(bucket_name, file_name):
-    #adding comment in function
-    print("Inside function")
-    print(bucket_name)
-    print(file_name)
-    
-    #buck=boto3.client('s3')
-    #obj=buck.get_object(Bucket=bucket_name,Key=file_name)
-    
-    #base64str = obj['Body'].read()
-    #print("MY BASE 64 STR: " ,base64str) 
     
     client = boto3.client('rekognition')
     response = client.detect_labels(
@@ -33,11 +19,6 @@ def rekognition_function(bucket_name, file_name):
         }, 
         MaxLabels=10 
     )
-    
-    #response = client.detect_labels(Image={'Bytes': base64str})
-    
-    #res_json = json.loads(obj["Body"].read())
-    #print("OBJECT IS" ,obj["Body"])    
     
     label_names = []
 
@@ -61,15 +42,8 @@ def store_json_elastic_search(json_object):
     url2= 'https://search-photos-seypxxu7hgwsmp4d3nqhcnd7oy.us-east-1.es.amazonaws.com/'
     
     resp = requests.post(url,auth=('cloudproj', 'Cloud@123'), data = json.dumps(json_object),headers = headers)
-    #url2 = 'https://vpc-photos-texbo7x2mjorrx3njqrmkiacha.us-east-1.es.amazonaws.com/photos/_search?pretty=true&q=*:*'
-    #resp_elastic = requests.get(url2,auth=awsauth,headers={"Content-Type": "application/json"}).json()
-    #print('<------------------GET-------------------->')
-    #print(json.dumps(resp_elastic, indent=4, sort_keys=True))
-    print("Request posted")
-    print(resp.text)
 
 def lambda_handler(event, context):
-    # TODO implement
     s3 = boto3.client('s3')
     record = event['Records'][0]
     
@@ -79,10 +53,6 @@ def lambda_handler(event, context):
     file_name = s3Object['object']['key']
     key = s3Object['object']['key']
     
-    #obj = s3.Object(bucket, key)
-    #res = obj.get()['Body'].read().decode('utf-8')
-    #print(res)
-    
     response = s3.head_object(Bucket=bucket, Key=key)
     print("head_object : " , response)
     if response["Metadata"]:
@@ -91,8 +61,7 @@ def lambda_handler(event, context):
         customlabels = customlabels.split(',')
         customlabels = list(map(lambda x: x.lower(), customlabels))
     time_stamp = record['eventTime']
-    print("Timestamp is :")
-    print(time_stamp)
+
     label_names = rekognition_function(bucket, file_name)
     if response["Metadata"]:
         for cl in customlabels:
@@ -100,8 +69,7 @@ def lambda_handler(event, context):
             cl = cl.lower().strip()
             if cl not in label_names:
                 label_names.append(cl)
-    print("test")
-    print(label_names)
+    
     json_object = {
         'objectKey': s3Object['object']['key'],
         'bucket': bucket,
